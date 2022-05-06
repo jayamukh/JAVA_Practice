7d66c7d06d47a21351aa9903a368e462cf70f38c
jayamukh <jayamukherjee.csus@gmail.com>
2022-05-06 10:01:07 +0530

    Committing excercises

---
+++ asteroidsDestroyed.java
@@ -0,0 +1,15 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+public class asteroidsDestroyed {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
